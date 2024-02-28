a1149fa5dd11f52889b7eea182cf9f3b3af4c04f
Omar Benavides <omar.benavides.rmz@gmail.com>
2024-02-28 01:25:05 -0600

    [frontend] Add methods to fetch backend API

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,1 +1,2 @@
+export const BACKEND_API = import.meta.env.VITE_BACKEND_API || "";
 export const GITHUB_REPO_URL = import.meta.env.VITE_GITHUB_REPO_URL || "";
---
+++ frontend/src/services/pokedex.ts
@@ -0,0 +1,26 @@
+import { BACKEND_API } from "@/lib/constants";
+import { APIAllPokemonResponse, APIGetPokemonResponse } from "@/types";
+
+const POKEMON_API_URL = `${BACKEND_API}/pokemon`;
+
+export const fetchAllPokemon = async (): Promise<APIAllPokemonResponse> => {
+  try {
+    const res = await fetch(POKEMON_API_URL);
+    return res.json();
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+export async function fetchPokemon(
+  query: string | number
+): Promise<APIGetPokemonResponse> {
+  try {
+    const res = await fetch(`${POKEMON_API_URL}/pokemon/${query}`);
+    return res.json();
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
