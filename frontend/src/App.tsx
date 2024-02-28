b4bbc00c7e112fdd65ebfcbe8e81466e2fe5219b
Omar Benavides <omar.benavides.rmz@gmail.com>
2024-02-28 01:24:20 -0600

    [frontend] Install and set up react-query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { Header } from "@/components/layout/Header";
+import { QueryProvider } from "@/providers/QueryProvider";
 
 function App() {
   return (
-    <>
+    <QueryProvider>
       <Header />
-    </>
+    </QueryProvider>
   );
 }
 
---
+++ frontend/src/providers/QueryProvider.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+export const QueryProvider = ({ children }: { children: ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
